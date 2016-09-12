e5e4a32cd97c0a5d85630608693fd473b6a99fd1
kazuki-yoshida <kazuki-yoshida@cookpad.com>
2016-09-12 22:25:09 +0900

    Add log unicorn setting

--- ruby/unicorn_config.rb
+++ ruby/unicorn_config.rb
@@ -1,3 +1,8 @@
+app_dir = '/home/isucon/private_isu/webapp/ruby'
+
 worker_processes 1
 preload_app true
 listen "127.0.0.1:8080"
+pid "#{app_dir}/unicorn.pid"
+stderr_path "#{app_dir}/log/unicorn.err.log"
+stdout_path "#{app_dir}/log/unicorn.out.log"
